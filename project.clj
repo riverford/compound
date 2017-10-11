d3cfe66238407a2ed231954341271a9c81382548
Daniel Neal <danielneal@riverford.co.uk>
2017-10-11 09:23:21 +0100

    Update metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject compound "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject danielneal/compound "0.1.0-SNAPSHOT"
+  :description "A micro structure for reagent data"
+  :url "https://github.com/danielneal/compound"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]
