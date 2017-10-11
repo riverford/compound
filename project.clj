4998bf0873db49f4f49259479008df4a56e23dd3
Daniel Neal <danielneal@riverford.co.uk>
2017-10-11 09:03:45 +0100

    Add spec dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]])
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+                 [org.clojure/spec.alpha "0.1.134"]])
