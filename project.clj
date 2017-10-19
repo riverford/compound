46a06845ad9d11a8159a23ffd30ea85e1c43d426
Daniel Neal <danielneal@riverford.co.uk>
2017-10-19 10:43:35 +0100

    0.2.0 release

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject danielneal/compound "0.2.0-SNAPSHOT"
+(defproject danielneal/compound "0.2.0"
   :description "A micro structure for reagent data"
   :url "https://github.com/danielneal/compound"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]
-                 [org.clojure/test.check "0.10.0-alpha2"]
                  [org.clojure/spec.alpha "0.1.134"]])
